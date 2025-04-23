276680afb73c62181f6e1b25b000b3d604836742
Shavkat838 <boltayevshavkat216@gmail.com>
2025-04-23 11:23:16 +0500

    rasm heighti togirlandi

--- app/side-bar.tsx
+++ app/side-bar.tsx
@@ -44,6 +44,7 @@ async function getUser(){
           alt="person"
           width={259}
           height={260}
+          style={{height:"260px"}}
         />
         <div>
           <p className="font-medium  text-[18px] leading-1 text-white mt-[14px]">
