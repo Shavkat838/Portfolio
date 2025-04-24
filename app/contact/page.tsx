c4b24ab7a5a195a5176d07f4fef1309f259fcdf6
Shavkat838 <boltayevshavkat216@gmail.com>
2025-04-24 11:31:37 +0500

    qolgan 2 ta page ham responsiv

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -63,16 +63,16 @@ export default function ContactPage() {
   }
 
   return (
-    <div>
+    <div className="max-w-[912px]  w-[100%] px-[10px] sm:px-0">
       {users.length > 0 ? (
-        <div className="mt-[32px] mb-[32px] ">
-          <div className="max-w-[912px]  w-[100%]  h-[278px] flex flex-col   gap-[24px] ">
-            <div className="w-[175px]  h-[65px ]  flex flex-col gap-[8px]  ">
+        <div className="mt-[32px] mb-[32px]">
+          <div className="max-w-[912px]  w-[100%] flex flex-col   gap-[24px] ">
+            <div className="max-w-[175px] w-full  h-[65px]  flex flex-col gap-[8px]">
               <h1 className="text-white  font-bold text-[32px]">Bog`lanish</h1>
               <Image src={"/border.svg"} alt="photo" width={112} height={8} />
             </div>
-            <div className="w-[912px] h-[190px] flex justify-between ">
-              <div className="w-[290px] h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
+            <div className="max-w-[912px]  grid grid-cols-1 sm:grid-cols-3  space-y-3 ">
+              <div className="max-w-[350px] sm:max-w-[290px] w-full h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
                 <div className="w-[100%] h-[134px] flex flex-col items-center gap-[8px]">
                   <Image
                     src={"/gmail.svg"}
@@ -88,7 +88,7 @@ export default function ContactPage() {
                   </p>
                 </div>
               </div>
-              <div className="w-[290px] h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
+              <div className="max-w-[350px] sm:max-w-[290px] w-full h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
                 <div className="w-[100%] h-[134px] flex flex-col items-center gap-[8px]">
                   <Image
                     src={"/telegram.svg"}
@@ -106,7 +106,7 @@ export default function ContactPage() {
                   </Link>
                 </div>
               </div>
-              <div className="w-[290px] h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
+              <div className="max-w-[350px] sm:max-w-[290px] w-full h-[190px] flex items-center justify-center bg-[#1B1B1B]  rounded-[12px] border-1 border-[#FFFFFF40]    ">
                 <div className="w-[100%] h-[134px] flex flex-col items-center gap-[8px]">
                   <Image
                     src={"/contact.svg"}
@@ -124,41 +124,41 @@ export default function ContactPage() {
               </div>
             </div>
           </div>
-          <div className="w-[912px] h-[502px] flex flex-col mt-[64px] gap-[24px]">
-            <div className="w-[226px]  h-[65px ]  flex flex-col gap-[8px]  ">
+          <div className="max-w-[912px]  flex flex-col mt-[64px] gap-[24px]">
+            <div className="max-w-[226px] w-full  h-[65px]  flex flex-col gap-[8px]  ">
               <h1 className="text-white  font-bold text-[28px]">
                 So`rov yuborish
               </h1>
               <Image src={"/border.svg"} alt="photo" width={112} height={8} />
             </div>
-            <div className="w-[912px] h-[420px] bg-[#1B1B1B] rounded-[12px] p-[20px]">
-              <div className="w-[872px] h-[76px] flex items-center justify-between  ">
-                <div className="w-[420px] h-[76px] flex flex-col justify-between  ">
+            <div className="max-w-[912px] w-[100%]  bg-[#1B1B1B] rounded-[12px] p-[20px]">
+              <div className="max-w-[872px] w-[100%] flex flex-col gap-[12px] sm:gap-0 sm:flex-row items-center justify-between  ">
+                <div className="max-w-[420px] w-[100%]  flex flex-col gap-[4px] justify-between  ">
                   <p className="text-white  font-normal text-[16px]">
                     Ismingiz*
                   </p>
                   <input
                     value={fullname}
                     onChange={(e) => setFullname(e.target.value)}
-                    className="border-1 border-[#FFFFFF40] pl-[20px] w-[100%] text-gray-300 h-[44px] rounded-[8px] "
+                    className="border-1 border-[#FFFFFF40] pl-[20px] max-w-[100%] text-gray-300 h-[44px] rounded-[8px] "
                     type="text"
-                    placeholder="Azimov Adham"
+                    placeholder="Ism"
                   />
                 </div>
-                <div className="w-[420px] h-[76px] flex flex-col justify-between  ">
+                <div className="max-w-[420px] w-[100%]  flex flex-col gap-[4px] justify-between">
                   <p className="text-white  font-normal text-[16px]">
                     Manzilingiz*
                   </p>
                   <input
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="border-1 border-[#FFFFFF40] pl-[20px] w-[100%] text-gray-300 h-[44px] rounded-[8px] "
+                    className="border-1 border-[#FFFFFF40] pl-[20px] max-w-[420px] w-[100%] text-gray-300 h-[44px] rounded-[8px] "
                     type="email"
-                    placeholder="azimov@gmail.com"
+                    placeholder="Email"
                   />
                 </div>
               </div>
-              <div className="w-[872px] h-[224px] flex flex-col gap-[8px] mt-[20px]">
+              <div className="max-w-[872px] w-full flex flex-col gap-[8px] mt-[20px]">
                 <p className="text-white  font-normal text-[16px]">
                   Izohingiz*
                 </p>
@@ -166,7 +166,7 @@ export default function ContactPage() {
                   value={izoh}
                   onChange={(e) => setIzoh(e.target.value)}
                   placeholder="Izohingizni kiriting"
-                  className="w-[872px]  h-[192px] p-[10px] rounded-[8px] border-1 border-[#FFFFFF40] text-gray-300  font-normal text-[16px]  "
+                  className="max-w-[872px] w-full h-[160px]  sm:h-[192px] p-[10px] rounded-[8px] border-1 border-[#FFFFFF40] text-gray-300  font-normal text-[16px]  "
                 ></textarea>
               </div>
               <button
--- app/loyiha/page.tsx
+++ app/loyiha/page.tsx
@@ -38,41 +38,45 @@ export default function LoyihaPage() {
   }
 
   return (
-    <div className="mt-[32px]">
-      <div className="max-w-[912px] w-[100%]  h-[840px] ">
-        <div className="w-[154px] h-[64px] flex  flex-col gap-[8px]">
+    <div className="mt-[32px] max-w-[912px] w-[100%]">
+      <div className="max-w-[912px] w-[100%]  px-[10px] sm:px-0 ">
+        <div className="max-w-[154px] w-full h-[64px] flex  flex-col gap-[8px]">
           <h1 className="text-white font-bold text-[32px]">Loyihalar</h1>
           <Image src={"/border.svg"} alt="photo" width={112} height={8} />
         </div>
-        <div className="w-[912px] h-[44px]  mt-[24px] flex justify-between ">
-          <div className="w-[140px] rounded-[6px] border-1 border-[#FFFFFF40] h-[44px] bg-[#1B1B1B] flex items-center justify-center gap-[10px]">
+        <div className="max-w-[912px] w-full h-[44px]  mt-[24px] flex justify-between ">
+          <div className="max-w-[130px] w-full rounded-[6px] border-1 border-[#FFFFFF40] h-[44px] bg-[#1B1B1B] flex items-center justify-center gap-[10px]">
             <p className="text-[16px] font-medium text-white  ">Filtrlash</p>
             <Image src={"/filter.svg"} alt="photo" width={20} height={20} />
           </div>
-          <div className="w-[484px] h-[44px] flex justify-end">
-            <div className="w-[148px] rounded-[6px] border-1 border-[#FFFFFF40] h-[44px] bg-[#1B1B1B] flex items-center justify-center gap-[10px]">
+          <div className="max-w-[484px]  w-full h-[44px] flex justify-end">
+            <div className="max-w-[148px] w-full rounded-[6px] border-1 border-[#FFFFFF40] h-[44px] bg-[#1B1B1B] flex items-center justify-center gap-[10px]">
               <p className="text-[16px] font-medium text-white  ">Barchasi</p>
               <Image src={"/chevron.svg"} alt="photo" width={20} height={20} />
             </div>
           </div>
         </div>
-        <div className="max-w-[912px] w-full  flex flex-wrap   mt-[25px] gap-y-[60px] gap-x-[20px]  ">
+        <div className="max-w-[912px] w-full  grid grid-cols-1 sm:grid-cols-2  mt-[25px] gap-y-[60px] gap-x-[20px]  ">
           {projects &&
             projects.map((item, index) => (
-              <div key={index} className="w-[444px] ">
+              <div key={index} className="max-w-[444px] w-full ">
                 <Image
                   className="rounded-[8px] "
                   src={item.projectImage}
                   alt="photo"
                   width={444}
-                  style={{ height: "220px",width:"444px",backgroundSize:"100% 100%" }}
+                  style={{
+                    height: "220px",
+                    width: "444px",
+                    backgroundSize: "100% 100%",
+                  }}
                   height={220}
                 />
-                <div className="mt-[12px] w-[444px] flex justify-between  ">
+                <div className="mt-[12px] max-w-[444px] w-full flex justify-between">
                   <h1 className="text-white text-[20px] font-medium  ">
                     {item.projectName}
                   </h1>
-                  <div className="flex gap-[6px] items-center w-[73px] h-[24px]">
+                  <div className="flex gap-[6px] items-center max-w-[73px] w-full h-[24px]">
                     <p className="text-white text-[16px] font-medium ">
                       {capitalizeFirstLetter(item.projectRole)}
                     </p>
@@ -87,9 +91,12 @@ export default function LoyihaPage() {
                     ></div>
                   </div>
                 </div>
-                <div className="w-[444px]  mt-[12px] flex flex-wrap gap-[12px]">
-                  {item.projectFields.map((it,index) => (
-                    <p key={index} className="text-[#39965F] text-[16px] font-medium  ">
+                <div className="max-w-[444px] w-full  mt-[12px] flex flex-wrap gap-[12px]">
+                  {item.projectFields.map((it, index) => (
+                    <p
+                      key={index}
+                      className="text-[#39965F] text-[16px] font-medium  "
+                    >
                       #{it}
                     </p>
                   ))}
@@ -99,22 +106,22 @@ export default function LoyihaPage() {
         </div>
       </div>
 
-      <div className="w-[912px] h-[208px] flex items-center mb-[26px]  mt-[64px] pl-[20px] rounded-[12px] bg-[#1B1B1B]  ">
-        <Image src={"/tg_icon.svg"} alt="telegram" width={168} height={168} />
-        <div className="ml-[20px] w-[406px] h-[160px]    flex flex-col gap-[12px] ">
-          <h1 className="text-white text-[28px] font-bold ">Telegram kanal</h1>
-          <p className="text-[18px] w-[406px]  font-normal text-[#FFFFFF40] ">
-            Barcha loyihalarimni telegram kanalimda <br /> ham kuzatib
+      <div className="max-w-[300px] w-full sm:max-w-[912px] min-h-[600px] sm:h-[208px] mx-auto sm:mx-0 flex flex-col sm:flex-row items-center mb-[26px]  mt-[64px] p  sm:pl-[20px] rounded-[12px] bg-[#1B1B1B]  ">
+        <Image className="mt-[20px] sm:mt-0" src={"/tg_icon.svg"} alt="telegram" width={168} height={168} />
+        <div className="sm:ml-[20px] max-w-[406px] w-full flex flex-col items-center sm:items-start gap-[20px] sm:gap-[12px] ">
+          <h1 className="text-white text-[28px] mt-[10px] sm:mt-0 font-bold ">Telegram kanal</h1>
+          <p className="text-[18px] max-w-[406px] text-center sm:text-start  w-full  font-normal text-[#FFFFFF40] ">
+            Barcha loyihalarimni telegram kanalimda <br className="hidden sm:block" /> ham kuzatib
             borishingiz mumkin!
           </p>
           <Link href={"https://t.me/shavkat1_4"}>
-            <button className="w-[178px] h-[40px] cursor-pointer bg-[#39965F] rounded-[8px]  text-white text-[16px]  font-normal  flex items-center justify-center  ">
+            <button className="w-[178px]   h-[40px] cursor-pointer bg-[#39965F] rounded-[8px]  text-white text-[16px]  font-normal  flex items-center justify-center  ">
               Tashrif buyurish
             </button>
           </Link>
         </div>
-        <div className="relative  max-w-[280px] w-full ">
-          <div className="">
+        <div className="relative  max-w-[280px] mt-[30px]  sm:mt-0  w-full ">
+          <div className=" ">
             <Image src={"/qiyalik1.svg"} alt="photo" width={144} height={144} />
           </div>
           <div className="absolute top-[-10px] right-[-10px]">
